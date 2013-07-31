5737a3085c0927e89df7f8c4619ceae6f6abd3d7
Kevin-Wirth <kevin@kevin-wirth.de>
2013-07-31 16:43:51 +0200

    Cleanup & verschmelzung
    Teil 1 der Verschmeldzung von FMS und ZVEI sowie Datenbankänderung

--- push.php
+++ push.php
@@ -36,22 +36,20 @@ if($operationsart == 'konsole') {
 // Prüfung Sicherheitskey
 if($key != $security_key) die ("Sicherheitskey falsch!");
 
-// ZVEI
 if($typ == 'zvei') {
 
 	// Sonderbehandlung LZ Morsbach
 	if($zvei == 73591 or $zvei == 73592) {
-
 		// Prüfen ob letzte Alarmierung länger als 60 sekunden her
 		$locktime = time()-60;
-		$result = mysql_query('SELECT `eid` FROM `event-db` WHERE `zvei` = \'77777\' AND `time` > \''.$locktime.'\'');
+		$result = mysql_query('SELECT `hid-zvei` FROM `historie-zvei` WHERE `zvei` = \'77777\' AND `time` > \''.$locktime.'\'');
 		if (mysql_fetch_assoc($result) == FALSE) {
 
 			$o_zvei = $zvei;
 			$zvei = 77777;
-			
+				
 			// Alarm in DB schreiben und ID auslesen
-			mysql_query('INSERT INTO `event-db` (`eid`, `time`, `zvei`, `weckruf`) VALUES (NULL, \''.time().'\', \'77777\', \'0\')');
+			mysql_query('INSERT INTO `historie-zvei` (`hid-zvei`, `time`, `zvei`, `weckruf`) VALUES (NULL, \''.time().'\', \'77777\', \'0\')');
 			$eid = mysql_insert_id();
 
 			// Beginn log Eintrag
@@ -59,28 +57,28 @@ if($typ == 'zvei') {
 
 			// Warten
 			sleep(15);
-			
+				
 			$gs = FALSE;
 			$ks = FALSE;
 			$sirene = FALSE;
 			if($o_zvei == 73591) {
 				$ks = TRUE;
 				$locktime = time()-90;
-				$result = mysql_query('SELECT `eid` FROM `event-db` WHERE `zvei` = \'73592\' AND `time` > \''.$locktime.'\'');
+				$result = mysql_query('SELECT `hid-zvei` FROM `historie-zvei` WHERE `zvei` = \'73592\' AND `time` > \''.$locktime.'\'');
 				if (mysql_fetch_assoc($result) != FALSE) $gs = TRUE;
 			}
 			if($o_zvei == 73592) {
 				$gs = TRUE;
 				$locktime = time()-90;
-				$result = mysql_query('SELECT `eid` FROM `event-db` WHERE `zvei` = \'73591\' AND `time` > \''.$locktime.'\'');
+				$result = mysql_query('SELECT `hid-zvei` FROM `historie-zvei` WHERE `zvei` = \'73591\' AND `time` > \''.$locktime.'\'');
 				if (mysql_fetch_assoc($result) != FALSE) $ks = TRUE;
 			}
-			$result = mysql_query('SELECT `eid` FROM `event-db` WHERE `zvei` = \'73590\' AND `weckruf` = \'2\' AND `time` > \''.$locktime.'\'');
+			$result = mysql_query('SELECT `hid-zvei` FROM `historie-zvei` WHERE `zvei` = \'73590\' AND `weckruf` = \'2\' AND `time` > \''.$locktime.'\'');
 			if (mysql_fetch_assoc($result) != FALSE) $sirene = TRUE;
 
 			// Alarmierungsparameter erstellen
 			if($sirene == TRUE) {
-				$alarm_event = 'SIRENENALARM'; 
+				$alarm_event = 'SIRENENALARM';
 			} else {
 				$alarm_event = 'MELDERALARM';
 			}
@@ -90,159 +88,124 @@ if($typ == 'zvei') {
 			if($gs == TRUE) $alarm_text = $alarm_text.'grosse Schleife';
 			$alarm_text = $alarm_text.' - Alarmierungszeit: '.date('d.m.Y, H:i:s');
 			$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'Text des Sammelalarms: '.$alarm_event.' - '.$alarm_text.'\', 0)';
+			$value = $zvei;
 		}
 	}
 
 	if($zvei != 77777) {
-
 		// Alarm in DB schreiben, Prüfen ob letzte Alarmierung länger als 60 sekunden her und ID auslesen
 		$locktime = time()-60;
-		$result = mysql_query('SELECT `eid` FROM `event-db` WHERE `zvei` = \''.$zvei.'\' AND `time` > \''.$locktime.'\'');
-		mysql_query('INSERT INTO `event-db` (`eid`, `time`, `zvei`, `weckruf`) VALUES (NULL, \''.time().'\', \''.$zvei.'\', \''.$weckton.'\')');
+		$result = mysql_query('SELECT `hid-zvei` FROM `historie-zvei` WHERE `zvei` = \''.$zvei.'\' AND `time` > \''.$locktime.'\'');
+		mysql_query('INSERT INTO `historie-zvei` (`hid-zvei`, `time`, `zvei`, `weckruf`) VALUES (NULL, \''.time().'\', \''.$zvei.'\', \''.$weckton.'\')');
 		$eid = mysql_insert_id();
 		if (mysql_fetch_assoc($result) != FALSE) die ("Zeitsperre");
 
 		// Prüfen ob ZVEI vorhanden; FALSE->DIE; TRUE->Daten in Array
-		$result = mysql_query('SELECT `zvei`, `value` FROM `zvei-db` WHERE `zvei` = \''.$zvei.'\'');
+		$result = mysql_query('SELECT `value`, `bezeichnung` FROM `mapping` WHERE `value` = \''.$zvei.'\'');
 		$mapping = mysql_fetch_assoc($result);
 		if($mapping == FALSE) 	die ("ZVEI nicht bekannt!");
-		
+			
 		// Warten
 		sleep(5);
 
 		// Beginn log Eintrag
-		$log = 'INSERT INTO `log` (`lid` ,`eid` ,`time` ,`value` ,`error`) VALUES (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'Neue Alarmierung gestartet ('.$zvei.'/'.$mapping['value'].')\' ,0)';
+		$log = 'INSERT INTO `log` (`lid` ,`eid` ,`time` ,`value` ,`error`) VALUES (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'Neue Alarmierung gestartet ('.$zvei.'/'.$mapping['bezeichnung'].')\' ,0)';
 
 		// Alarmierungsparameter erstellen
 		$sirene = FALSE;
-		$result = mysql_query('SELECT `eid` FROM `event-db` WHERE `zvei` = \''.$zvei.'\' AND `weckruf` = \'2\' AND `time` > \''.$locktime.'\'');
+		$result = mysql_query('SELECT `hid-zvei` FROM `historie-zvei` WHERE `zvei` = \''.$zvei.'\' AND `weckruf` = \'2\' AND `time` > \''.$locktime.'\'');
 		if (mysql_fetch_assoc($result) != FALSE) $sirene = TRUE;
 		if($sirene == TRUE) {
 			$alarm_event = 'SIRENENALARM'; 
 		} else {
 			$alarm_event = 'MELDERALARM';
 		}
-		$alarm_text = $mapping['value'].' - Alarmierungszeit: '.date('d.m.Y, H:i:s');
+		$alarm_text = $mapping['bezeichnung'].' - Alarmierungszeit: '.date('d.m.Y, H:i:s');
 		$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'Alarmtext: '.$alarm_event.' - '.$alarm_text.'\', 0)';
-	}
-
-	// Alarmierungsempfänger auslesen + versenden
-	$result = mysql_query('SELECT `address`.`aid` AS `aid`, `user`.`uid` AS `uid`, `mid`, `type`, `value`, `username`, `zvei`, `prio` FROM `mapping` INNER JOIN `address` ON `address`.`aid` = `mapping`.`aid` INNER JOIN `user` ON `user`.`uid` = `address`.`uid` WHERE `zvei` = \''.$zvei.'\'');
-	while($alarm = mysql_fetch_assoc($result)) {
-		if($alarm['type'] == 'prowl') {
-			$prowl_error = null;
-
-			try {
-				$config = array(
-					'apiKey' => $alarm['value'], // provide an API key to test
-					'apiProviderKey' => $prowl_providerKey,
-				);
-				$prowl = new Prowl($config);
-
-				$notification = array(
-					'application' => $prowl_application,
-					'event' => $alarm_event,
-					'description' => $alarm_text,
-					'url' => $prowl_url,
-					'priority'  => $alarm['prio']
-				);
-
-				$message = $prowl->add($notification);	
-				
-			} catch (Exception $message) {
-				$prowl_error = 'Fehler: ' . $message->getMessage();
-			}
-
-			if ($prowl_error != null) {
-				$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'User: '.$alarm['username'].' (uid: '.$alarm['uid'].', aid: '.$alarm['aid'].', mid: '.$alarm['mid'].'); '.$prowl_error.'\', 1)';
-			} else {
-				$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'User: '.$alarm['username'].' (uid: '.$alarm['uid'].', aid: '.$alarm['aid'].', mid: '.$alarm['mid'].'); Alarmierung wurde erfolgreich verschickt!\', 0)';
-			}
-		} elseif($alarm['type'] == 'sms') {
-			$url = 'http://sms77.de/gateway/' .
-				'?u=' . urlencode($sms_u) .
-				'&p=' . urlencode($sms_p) .
-				'&to=' . urlencode($alarm['value']) .
-				'&text=' . urlencode($alarm_event.': '.$alarm_text) .
-				'&type=' . urlencode($sms_type) .
-				'&from=' . urlencode($sms_from);
-
-			$ret = @file($url);
-
-			if ($ret[0] == '100') {
-				$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'User: '.$alarm['username'].' (uid: '.$alarm['uid'].', aid: '.$alarm['aid'].', mid: '.$alarm['mid'].'); SMS-Alarmierung wurde erfolgreich verschickt!\', 0)';
-			} else {
-				$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'User: '.$alarm['username'].' (uid: '.$alarm['uid'].', aid: '.$alarm['aid'].', mid: '.$alarm['mid'].'); Fehler beim SMS-Versand! Fehlercode: '.$ret[0].' ('.$sms_err_code[$ret[0]].')\', 1)';
-			}
-		} elseif($alarm['type'] == 'aapp') {
-			unset($aapp_ret);
-			exec($aapp_pfad . 'AlarmPushTool.exe -c '.$zvei.' -t "'.$alarm_event.'" -m "'.$alarm_text.'" -p '.$aapp_pfad, $aapp_ret);
-
-			$aapp_log = 'AlarmApp Rückmeldung: ';
-			foreach ($aapp_ret as $value) {
-				$aapp_log = $aapp_log.$value;
-			}
-			unset($value);
-
-			$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\''.$aapp_log.'\', 0)';
-		}
-	}
-
-	// log Einträge speichern
-	mysql_query($log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'Alarmverarbeitung beendet.\' ,0)');
+		$value = $zvei;
+	}	
 	
-// FMS
-} else {
+} elseif($typ='fms') {
 
-	$fms_list = array(
-	'69692810'	=>	'Morsbach 1-KDOW',
-	'69692819'	=>	'Morsbach 1-MTF',
-	'69692831'	=>	'Morsbach 1-LF20',
-	'69692817'	=>	'Morsbach 1-HLF20',
-	'69692847'	=>	'Morsbach 1-LF16TS',
-	'69692802'	=>	'Morsbach 1-WLF',
-	'69692891'	=>	'Morsbach 1-GWG',
-	'69692919'	=>	'Morsbach 2-MTF',
-	'69692852'	=>	'Morsbach 2-TSF',
-	'69692940'	=>	'Morsbach 2-LF10',
-	'69692914'	=>	'Morsbach 2-RW1',
-	'69693019'	=>	'Morsbach 3-MTF',
-	'69693040'	=>	'Morsbach 3-LF10',
-	'69693119'	=>	'Morsbach 4-MTF',
-	'69693140'	=>	'Morsbach 4-HLF10'
-	);
+	$locktime = time()-60;
+	mysql_query('INSERT INTO `historie-fms` (`id`, `kennung`, `status`, `time`) VALUES (NULL, \''.$fms.'\', \''.$status.'\',\''.time().'\')');
+	$eid = mysql_insert_id();
+	$result = mysql_query('SELECT `id` FROM `historie-fms` WHERE `kennung` = \''.$fms.'\' AND `status` =\''.$status.'\' AND `time` > \''.$locktime.'\' ORDER BY `id` ASC LIMIT 0,1');
+	$id_res = mysql_fetch_array($result);
+	if ($id_res['id'] != $eid) die ("Zeitsperre");
+	
+	$result = mysql_query('SELECT `value`, `bezeichnung` FROM `mapping` WHERE `value` = \''.$fms.'\'');
+	$mapping = mysql_fetch_assoc($result);
+	if($mapping == FALSE) 	die ("FMS-Kennung nicht bekannt!");
 
-	if (array_key_exists($fms, $fms_list)) {
-		mysql_query('INSERT INTO `fms` (`id`, `kennung`, `status`, `time`) VALUES (NULL, \''.$fms.'\', \''.$status.'\',\''.time().'\')');
-		$status_id = mysql_insert_id();
-		$locktime = time()-60;
-		$result = mysql_query('SELECT `id` FROM `fms` WHERE `kennung` = \''.$fms.'\' AND `status` =\''.$status.'\' AND `time` > \''.$locktime.'\' ORDER BY `id` ASC LIMIT 0,1');
-		$id_res = mysql_fetch_array($result);
-		if ($id_res['id'] == $status_id) {
-			mysql_query('INSERT INTO `fms` (`id`, `kennung`, `status`, `time`) VALUES (NULL, \''.$fms.'\', \''.$status.'\',\''.time().'\')');
-
-				$config = array(
-					'apiKey' => 'fe44f426f24997d7092553cc5d6c7b4a1f854c80',
-					'apiProviderKey' => $prowl_providerKey,
-				);
-				$prowl = new Prowl($config);
-
-				$notification = array(
-					'application' => 'FMS-Info',
-					'event' => $fms_list[$fms],
-					'description' => 'Status: '.$status.' - '.date('d.m.Y, H:i:s'),
-					'url' => $prowl_url,
-					'priority'  => '-2'
-				);
-
-				$message = $prowl->add($notification);
-		} else {
-			die;
-		}
-	} else {
-		die;
-	}
-
+	$alarm_event = $mapping['bezeichnung']; 
+	$alarm_text = 'Status: '.$status.' - '.date('d.m.Y, H:i:s');
+	$value = $fms;
 }
+
+// Alarmierungsempfänger auslesen + versenden
+$result = mysql_query('SELECT `user`.`uid` AS `uid`, `username`, `typ`, `nummer`, `re-id`, `emid`, `value`, `prio` FROM `event-mapping` INNER JOIN `user` ON `event-mapping`.`uid` = `user`.`uid` WHERE `value` = \''.$value.'\'');
+while($alarm = mysql_fetch_assoc($result)) {
+	if($alarm['typ'] == 'prowl') {
+		$prowl_error = null;
+
+		try {
+			$config = array(
+				'apiKey' => $alarm['nummer'], // provide an API key to test
+				'apiProviderKey' => $prowl_providerKey,
+			);
+			$prowl = new Prowl($config);
+
+			$notification = array(
+				'application' => $prowl_application,
+				'event' => $alarm_event,
+				'description' => $alarm_text,
+				'url' => $prowl_url,
+				'priority'  => $alarm['prio']
+			);
+
+			$message = $prowl->add($notification);	
+				
+		} catch (Exception $message) {
+			$prowl_error = 'Fehler: ' . $message->getMessage();
+		}
+
+		if ($prowl_error != null) {
+			$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'User: '.$alarm['username'].' (uid: '.$alarm['uid'].', emid: '.$alarm['emid'].'); '.$prowl_error.'\', 1)';
+		} else {
+			$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'User: '.$alarm['username'].' (uid: '.$alarm['uid'].', emid: '.$alarm['emid'].'); Alarmierung wurde erfolgreich verschickt!\', 0)';
+		}
+	} elseif($alarm['typ'] == 'sms') {
+		$url = 'http://sms77.de/gateway/' .
+			'?u=' . urlencode($sms_u) .
+			'&p=' . urlencode($sms_p) .
+			'&to=' . urlencode($alarm['nummer']) .
+			'&text=' . urlencode($alarm_event.': '.$alarm_text) .
+			'&type=' . urlencode($sms_type) .
+			'&from=' . urlencode($sms_from);
+
+		$ret = @file($url);
+
+		if ($ret[0] == '100') {
+			$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'User: '.$alarm['username'].' (uid: '.$alarm['uid'].', emid: '.$alarm['emid'].'); SMS-Alarmierung wurde erfolgreich verschickt!\', 0)';
+		} else {
+			$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'User: '.$alarm['username'].' (uid: '.$alarm['uid'].', emid: '.$alarm['emid'].'); Fehler beim SMS-Versand! Fehlercode: '.$ret[0].' ('.$sms_err_code[$ret[0]].')\', 1)';
+		}
+	} elseif($alarm['typ'] == 'aapp') {
+		unset($aapp_ret);
+		exec($aapp_pfad . 'AlarmPushTool.exe -c '.$zvei.' -t "'.$alarm_event.'" -m "'.$alarm_text.'" -p '.$aapp_pfad, $aapp_ret);
+
+		$aapp_log = 'AlarmApp Rückmeldung: ';
+		foreach ($aapp_ret as $value) {
+			$aapp_log = $aapp_log.$value;
+		}
+		unset($value);
+		
+		$log = $log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\''.$aapp_log.'\', 0)';
+	}
+}
+
+// log Einträge speichern
+mysql_query($log.', (NULL ,\''.$eid.'\' ,\''.time().'\' ,\'Alarmverarbeitung beendet.\' ,0)');
 
 ?>
